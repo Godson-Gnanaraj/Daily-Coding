f31caa530d13ac2f6eb61d6ed30c9129ef1031db
Godson <lgodsongnanaraj@gmail.com>
2020-06-30 17:25:36 +0530

    Day 1 solution

---
+++ Day1/two-sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def two_sum(self, nums, k):
+        nums.sort()
+        start, end = 0, len(nums) - 1
+        while start < end:
+            sum_val = nums[start] + nums[end]
+            if sum_val == k:
+                return True
+            elif sum_val > k:
+                end -= 1
+            else:
+                start += 1
+        return False
+
+
+def main():
+    nums = [10, 15, 3, 7]
+    k = 20
+    solution = Solution()
+    ans = solution.two_sum(nums, k)
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
